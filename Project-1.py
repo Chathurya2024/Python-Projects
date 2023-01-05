4ebbf317b0a16beaacc10c5b596cc5f423a4ef06
chathurya0 <122029749+chathurya0@users.noreply.github.com>
2023-01-05 19:36:31 +0530

    Add files via upload

---
+++ Project-1.py
@@ -0,0 +1,19 @@
+#Project - 1
+#Brand Name Generator program.
+
+print("Hello there!") 
+#or print('Hello there!')
+
+#Taking input from the user
+city_name = input("Which city were you bought up? \n") 
+pet_name = input("What is your pet name? \n")
+#\n makes sure the input cursor shows on a new line:
+
+#combining the name of their city and pet and show them their band name
+print("Your band name could be " + city_name + " " + pet_name)
+
+
+
+
+
+
